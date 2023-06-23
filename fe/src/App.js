0992c1ee795039f505bfaf5e6ceb7ffff03ea8ae
Arvind <nairarvind2011@gmail.com>
2023-06-24 00:18:20 +0530

    Adds bootstrap and variables to highlight selections

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,85 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
-
-import { useState, useRef, useEffect } from 'react';
-
-import axios, * as others from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
     const [artists, setArtists] = useState([]);
-    const [tracks, setTracks] = useState([])
-    const [lyrics, setLyrics] = useState([])
-
+    const [selectedArtist, setSelectedArtist] = useState(null);
+    const [tracks, setTracks] = useState([]);
+    const [selectedTrack, setSelectedTrack] = useState(null);
+    const [lyrics, setLyrics] = useState([]);
 
     useEffect(() => {
-        axios.get("http://127.0.0.1:8000//api/v1/artist")
-            .then((resp) => {
-                setArtists(resp.data.artists);
-                setTracks([])
-                setLyrics([])
-            });
+        axios.get("http://127.0.0.1:8000/api/v1/artist").then((response) => {
+            setArtists(response.data.artists);
+        });
     }, []);
 
+    useEffect(() => {
+        if (selectedArtist !== null) {
+            axios
+                .get(`http://127.0.0.1:8000/api/v1/artist/${selectedArtist}`)
+                .then((response) => {
+                    setTracks(response.data.tracks);
+                });
+        }
+    }, [selectedArtist]);
 
-    function onClickHandlerTracks(e) {
-        e.preventDefault();
-        const artistId = e.currentTarget.getAttribute('artist_id');
-        axios.get(`http://127.0.0.1:8000/api/v1/artist/${artistId}`)
-            .then((resp) => {
-                setTracks(resp.data.tracks);
-                setLyrics([])
-            });
+    useEffect(() => {
+        if (selectedTrack !== null) {
+            axios
+                .get(`http://127.0.0.1:8000/api/v1/song/${selectedTrack}`)
+                .then((response) => {
+                    setLyrics([response.data]);
+                });
+        }
+    }, [selectedTrack]);
+
+    function handleArtistClick(id) {
+        setSelectedArtist(id);
+        setSelectedTrack(null);
+        setTracks([]);
+        setLyrics([]);
     }
 
-
-    function onClickHandlerLyrics(e) {
-        e.preventDefault()
-        const trackId = e.currentTarget.getAttribute('track_id')
-        // console.log(trackId)
-        axios.get(`http://127.0.0.1:8000/api/v1/song/${trackId}`)
-            .then((resp) => {
-                setLyrics([resp.data])
-                console.log(resp.data)
-            })
+    function handleTrackClick(id) {
+        setSelectedTrack(id);
+        setLyrics([]);
     }
-
 
     return (
-        <div className="row">
-            <div className="col">
-                <h2> Artists </h2>
-                <ol>
-                    {artists.map(((artist, idx) => <li key={`artist${artist.id}`}>
-                        <a
-                            href={`http://127.0.0.1:8000/api/v1/artist/${artist.id}`}
-                            onClick={onClickHandlerTracks}
-                            artist_id={artist.id}
-                        >{artist.name}
-                        </a>
-                    </li>))}
-                </ol>
-            </div>
-            <div className="col">
-                <h2> Tracks </h2>
-                <ul>
-                    {tracks.map(((track, idx) => <li key={`track${track.id}`}>
-                        <a
-                            href={`http://127.0.0.1:8000/api/v1/song/${track.id}`}
-                            onClick={onClickHandlerLyrics}
-                            track_id={track.id}
-                        >{track.name}
-                        </a>
-                    </li>))}
-                </ul>
-            </div>
-            <div className="col">
-                <h2> Lyrics </h2>
-                {lyrics.map(((lyric, idx) => 
-                <div key={idx}>
-                    <div>{lyric.name}</div>
-                    <div style={{ whiteSpace: 'pre-line' }}>{lyric.lyrics}</div>
-                </div>))}
-
+        <div className="container-fluid bg-light p-5">
+            <h1 className="text-center display-2 text-primary mb-5">Hamon Music Database</h1>
+            <div className="row justify-content-center align-items-start">
+                <div className="col-md-4 mb-5">
+                    <h2 className="text-center display-4 text-dark mb-4">Artists</h2>
+                    <div className="list-group">
+                        {artists.map((artist) => (
+                            <button
+                                key={artist.id}
+                                type="button"
+                                className={`list-group-item list-group-item-action rounded-lg ${selectedArtist === artist.id ? "active" : ""
+                                    }`}
+                                onClick={() => handleArtistClick(artist.id)}
+                            >
+                                {artist.name}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+                <div className="col-md-8">
+                    {selectedArtist !== null && (
+                        <div>
+                            <h2 className="text-center display-4 text-dark mb-4">
+                                Tracks by {artists.find((a) => a.id === selectedArtist).name}
+                            </h2>
+                            <ul className="list-group">
+                                {tracks.length > 0 ? (
+                                    tracks.map((track) => (
+                                        <button
+                                            key={track.id}
+                                            type="button"
+                                            className={`list-group-item list-group-item-action rounded-lg ${selectedTrack === track.id ? "active" : ""
+                                                }`}
+                                            onClick={() => handleTrackClick(track.id)}
+                                        >
+                                            <div className="d-flex justify-content-between align-items-center">
+                                                <span className="text-center">{track.name}</span>
+                                            </div>
+                                        </button>
+                                    ))
+                                ) : (
+                                    <p className="text-center text-muted my-5">
+                                        No tracks to display.
+                                    </p>
+                                )}
+                            </ul>
+                        </div>
+                    )}
+                    {selectedTrack !== null && (
+                        <div className="mt-5">
+                            <h2 className="text-center display-4 text-dark mb-4">
+                                Lyrics for{" "}
+                                {tracks.find((t) => t.id === selectedTrack).name}
+                            </h2>
+                            {lyrics.map((lyric) => (
+                                <div key={lyric.id}>
+                                    <p className="text-center text-dark h6" style={{ whiteSpace: 'pre-line' }}>{lyric.lyrics}</p>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
