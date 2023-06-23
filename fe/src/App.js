634e0b5e52a9877facf0eee0cb685ca8a290a520
Noufal Ibrahim <noufal@nibrahim.net.in>
2023-06-23 11:17:57 +0530

    Starting react app

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,23 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    // return (<div></div>
+    // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="row">
+          <div className="col">
+          <h2> Artists </h2>
+          </div>
+          <div className="col">
+          <h2> Tracks </h2>
+          </div>
+          <div className="col">
+          <h2> Lyrics </h2>
+          </div>
+          </div>
   );
 }
 
