3a0704f0fce5a6725641e7557d32de9887bf732c
Arvind <nairarvind2011@gmail.com>
2023-06-25 15:48:22 +0530

    Removes unused code from web.py

--- lyrics/web.py
+++ lyrics/web.py
@@ -1,6 +1,4 @@
-import time
-
-from flask import Flask, Response, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 
 import models
@@ -26,7 +24,6 @@ def api_artist(artist_id):
     artist = db.session.execute(
         db.select(models.Artist).filter(models.Artist.id == artist_id)
     ).scalar()
-    print(dir(artist))
     ret = [{"id": i.id, "name": i.name} for i in artist.tracks]
     return jsonify(dict(tracks=ret))
 
@@ -39,59 +36,24 @@ def song(song_id):
     track = db.session.execute(
         db.select(models.Tracks).filter(models.Tracks.id == song_id)
     ).scalar()
-    # track.lyrics = track.lyrics.replace("\n", "<br/>")
     lyrics = {"name": track.name, "lyrics": track.lyrics}
     return jsonify(lyrics)
 
 
-@app.route('/api/v1/initdb', methods=['POST'])
+@app.route("/api/v1/initdb", methods=["POST"])
 def init_db():
     db = models.init_db(app, "postgresql:///lyrics")
     with app.app_context():
         db.drop_all()
         db.create_all()
-    return 'Initialized the database'
+    return "Initialized the database"
 
-# Add route for Crawl command
-@app.route('/api/v1/crawl', methods=['POST'])
+
+@app.route("/api/v1/crawl", methods=["POST"])
 def crawl():
-    nartists = request.args.get('nartists', default=8, type=int)
-    ntracks = request.args.get('ntracks', default=5, type=int)
-    crawler.crawl("https://www.songlyrics.com/top-artists-lyrics.html", nartists, ntracks)
-    return 'Crawled lyrics'
-
-@app.route("/")
-def index():
-    db = models.init_db(app)
-    artists = db.session.execute(db.select(models.Artist)).scalars()
-    return render_template("index.html", artists=artists)
-
-
-@app.route("/artist/<artist_id>")
-def artist(artist_id):
-    db = models.init_db(app)
-    artists = db.session.execute(db.select(models.Artist)).scalars()
-    artist = db.session.execute(
-        db.select(models.Artist).filter(models.Artist.id == artist_id)
-    ).scalar()
-    return render_template("artists.html", artists=artists, current=artist)
-
-
-@app.route("/songs/<artist_id>")
-def songs(artist_id):
-    db = models.init_db(app)
-    artist = db.session.execute(
-        db.select(models.Artist).filter(models.Artist.id == artist_id)
-    ).scalar()
-    tracks = []
-    for i in artist.tracks:
-        t = {"id": i.id, "name": i.name, "lyrics": i.lyrics}
-        tracks.append(t)
-
-    ret = {"current": 0, "tracks": tracks}
-    return jsonify(ret)
-
-
-@app.route("/user/<id>")
-def users(id):
-    return f"You asked for user {id}"
+    nartists = request.args.get("nartists", default=8, type=int)
+    ntracks = request.args.get("ntracks", default=5, type=int)
+    crawler.crawl(
+        "https://www.songlyrics.com/top-artists-lyrics.html", nartists, ntracks
+    )
+    return "Crawled lyrics"
