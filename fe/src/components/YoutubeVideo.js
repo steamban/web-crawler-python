ca622bdf4c8691e20716212f2ec5a6d858951976
Arvind <nairarvind2011@gmail.com>
2023-06-25 11:46:32 +0530

    Creates new component to embed yt in webpage

---
+++ fe/src/components/YoutubeVideo.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function YoutubeVideo({ artistName, trackName }) {
+    const [videoId, setVideoId] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${encodeURIComponent(
+                        `${artistName} ${trackName}`
+                    )}&key=[API_KEY_HERE]`
+                );
+                if (response?.data?.items?.length) {
+                    setVideoId(response.data.items[0].id.videoId);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, [artistName, trackName]);
+
+    return (
+        <div className="mt-5 mb-5 text-center">
+        {videoId && (
+          <iframe
+            className="mx-auto d-block"
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title={`${artistName} - ${trackName}`}
+            allowFullScreen
+          ></iframe>
+        )}
+      </div>
+    );
+}
+
+export default YoutubeVideo;
