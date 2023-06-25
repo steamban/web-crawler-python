9830e5f3531e53cea31a2cfdcb4a7e0d812d3ce5
Arvind <nairarvind2011@gmail.com>
2023-06-25 16:01:26 +0530

    Removes unused code from cli.py

--- lyrics/cli.py
+++ lyrics/cli.py
@@ -1,47 +1,50 @@
 import argparse
-import logging
 
 import crawler
 import models
 import utils
 import web
 
-import sqlalchemy as sa
-from sqlalchemy.orm import Session
-
 logger = utils.get_logger()
+
 
 def parse():
     parser = argparse.ArgumentParser(
-        prog = "lyrics",
-        description = "Offline song lyrics browser")
-    
-    parser.add_argument("-d", "--debug", help = "Display detailed debug", action="store_true", default=False)
-    
+        prog="lyrics", description="Offline song lyrics browser"
+    )
+
+    parser.add_argument(
+        "-d",
+        "--debug",
+        help="Display detailed debug",
+        action="store_true",
+        default=False,
+    )
+
     subparsers = parser.add_subparsers(dest="command")
-    subparsers.add_parser("web", help = "Run web server")
 
-    subparsers.add_parser("listartists", help = "List of artists in the system")
-    subparsers.add_parser("initdb", help = "Initialise the database")
-    crawl_parser = subparsers.add_parser("crawl", help = "Crawl lyrics")
+    subparsers.add_parser("web", help="Run web server")
+    subparsers.add_parser("initdb", help="Initialise the database")
 
-    crawl_parser.add_argument("--nartists", help="Number of artists to crawl (Default : %(default)s)", 
-                              type=int, 
-                              default=8)
+    crawl_parser = subparsers.add_parser("crawl", help="Crawl lyrics")
 
-    crawl_parser.add_argument("--ntracks", help="Number of tracks to crawl per artist (Default : %(default)s)",
-                              type=int,
-                              default=5)
+    crawl_parser.add_argument(
+        "--nartists",
+        help="Number of artists to crawl (Default : %(default)s)",
+        type=int,
+        default=8,
+    )
+
+    crawl_parser.add_argument(
+        "--ntracks",
+        help="Number of tracks to crawl per artist (Default : %(default)s)",
+        type=int,
+        default=5,
+    )
 
     args = parser.parse_args()
     return args
 
-def handle_listartists(args):
-    db = models.init_db(web.app, "postgresql:///lyrics")
-    with web.app.app_context():
-        artists = db.session.execute(db.select(models.Artist)).scalars()
-        for idx,artist in enumerate(artists, start=1):
-            print (f"{idx}. {artist.name}")
 
 def handle_initdb(args):
     db = models.init_db(web.app, "postgresql:///lyrics")
@@ -49,37 +52,32 @@ def handle_initdb(args):
         db.drop_all()
         db.create_all()
 
+
 def handle_crawl(args):
     db = models.init_db(web.app, "postgresql:///lyrics")
-    crawler.crawl("https://www.songlyrics.com/top-artists-lyrics.html", 
-                    args.nartists, 
-                    args.ntracks)
+    crawler.crawl(
+        "https://www.songlyrics.com/top-artists-lyrics.html",
+        args.nartists,
+        args.ntracks,
+    )
 
-def handle_test(args):
-    engine = sa.create_engine("postgresql:///lyrics", echo=True)
-    query= sa.select(models.Artists)
-    with Session(engine) as sess:
-        results = sess.scalars(query)
-        for artist in results:
-            print (artist.name)
-            for song in artist.songs:
-                print("   ", song.name)
 
 def handle_web(args):
     db = models.init_db(web.app, "postgresql:///lyrics")
     web.app.run(host="0.0.0.0", port=8000, debug=True)
 
 
-
 def main():
-    commands = {"listartists" : handle_listartists,
-                "initdb"  : handle_initdb ,
-                "crawl" : handle_crawl,
-                "web" : handle_web}
+    commands = {
+        "initdb": handle_initdb,
+        "crawl": handle_crawl,
+        "web": handle_web,
+    }
 
     args = parse()
     utils.setup_logger(args.debug)
     commands[args.command](args)
+
 
 if __name__ == "__main__":
     main()
